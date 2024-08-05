866bc41f708bd46a1e1af25fd6328a5d0d101b88
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-08-05 14:11:05 -0300

    Create Name_hello.py

---
+++ Project the graduation in code-(Cybersegurity)/Python/Programming Language/3/Name_hello.py
@@ -0,0 +1,8 @@
+nome = input()
+
+print(nome)
+
+#formatadores de caracteres
+print("Olá, %s, bem-vindo à disciplina de programação. Parabéns pelo seu primeiro hello world" % (nome))
+
+print(f{nome}, bem-vindo à disciplina de programação. Parabéns pelo seu primeiro hello world")
