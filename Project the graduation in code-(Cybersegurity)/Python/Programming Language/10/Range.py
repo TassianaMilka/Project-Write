9b095d378aa0d4175887b76d916874daed055f59
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-08-06 16:01:23 -0300

    Update Range.py

--- Project the graduation in code-(Cybersegurity)/Python/Programming Language/10/Range.py
+++ Project the graduation in code-(Cybersegurity)/Python/Programming Language/10/Range.py
@@ -53,4 +53,44 @@ for numero in range(1, 11):
   print(numero)
 
 
+/*6*/
 
+filmes = [, , , , ]
+
+ 
+
+print()
+
+print()
+
+print()
+
+ 
+
+for filme in filmes:
+
+    while True:
+
+        classificacao = input(f{filme}' de 1 a 5? (ou 0 para parar): ")
+
+        if classificacao == '0':
+
+            print(f{filme}' interrompida.")
+
+            break  # Encerra o loop interno com "break"
+
+        classificacao = int(classificacao)
+
+        if classificacao < 1 or classificacao > 5:
+
+            print()
+
+        else:
+
+            print(f{filme}' com {classificacao} estrelas.\n")
+
+            break  # Sai do loop interno
+
+ 
+
+print()
