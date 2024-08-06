9a6df5b2569e85784ea746c883323123f5ac2dc8
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-08-06 15:30:35 -0300

    Create Range.py

---
+++ Project the graduation in code-(Cybersegurity)/Python/Programming Language/10/Range.py
@@ -0,0 +1,32 @@
+
+/*1*/
+
+for x in range(5):
+
+    print(x)
+
+
+/*2*/
+
+for y in range(2, 7):
+
+    print(y)
+
+
+/*3*/
+
+for z in range(1, 11, 2):
+
+    print(z)
+
+
+/*4*/
+
+for numero in range(1, 11):
+
+    if numero % 2 == 0:
+
+        print(, numero)
+
+        break
+
