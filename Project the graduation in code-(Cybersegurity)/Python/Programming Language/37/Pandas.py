78daef68dcc9a68f8d1cafa22a13ddf9533c5a97
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-10 16:38:40 -0300

    Update Pandas.py

--- Project the graduation in code-(Cybersegurity)/Python/Programming Language/37/Pandas.py
+++ Project the graduation in code-(Cybersegurity)/Python/Programming Language/37/Pandas.py
@@ -35,4 +35,29 @@ series2 = pd.Series(data)
 
  
 
+print(series2) 
+
+
+
+
+import pandas as pd
+
+ 
+
+# Criando um dicionário com pares chave-valor
+
+data = {'A': 100, 'B': 200, 'C': 300, 'D': 400, 'E': 500}
+
+ 
+
+# Criando uma Series a partir do dicionário
+
+series2 = pd.Series(data)
+
+ 
+
 print(series2)
+
+
+
+
